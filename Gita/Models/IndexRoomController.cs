49fb3c00eff50628c319c380be7b9903d06f71d9
Dr. Ghorai <Dr. Ghorai@DRGHORAI>
2016-11-18 12:37:00 +0530

    Add project files.

---
+++ Gita/Models/IndexRoomController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gita.Models
+{
+    public class IndexRoomController : Controller
+    {
+        // GET: IndexRoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gita/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gita.Startup))]
+namespace Gita
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
